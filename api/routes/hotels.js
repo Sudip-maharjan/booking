3dc2c207064ad8a58bf76820a197dad4f7c6d209
Sudip-maharjan <maharjansudip2060@gmail.com>
2025-10-08 23:53:24 +0545

    databse CRUD operations

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -18,9 +18,13 @@ router.post("/", async (req, res) => {
 //update
 router.put("/:id", async (req, res) => {
   try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
-      $set: req.body,
-    });
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
     res.status(200).json(updatedHotel);
   } catch (err) {
     res.status(500).json(err);
@@ -28,7 +32,33 @@ router.put("/:id", async (req, res) => {
 });
 
 //delete
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel is deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get all
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
