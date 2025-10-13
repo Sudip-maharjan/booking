45afab87b054c6da7c7d49080283b3b79f49543f
Sudip-maharjan <maharjansudip2060@gmail.com>
2025-10-13 22:05:00 +0545

    User and Admin role checking

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,20 @@ import {
   getHotels,
   updateHotel,
 } from "../controllers/hotel.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/checkauthentication", verifyToken, (req, res, next) => {
   res.send("hello user, you are logged in");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("hello user, you are logged in and you can delete your account");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("hello Admin, you are logged in and you can delete all account");
 });
 
 //update
